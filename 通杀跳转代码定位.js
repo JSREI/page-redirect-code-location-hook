ec27b2ae8ed42b99647fe01c7d599b114bc29ccc
CC11001100 <CC11001100@qq.com>
2022-07-27 00:57:51 +0800

    feat: add license

--- 通杀跳转代码定位.js
+++ 通杀跳转代码定位.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @name         页面跳转阻断
-// @namespace    https://github.com/CC11001100/crawler-js-hook-framework-public/tree/master/011-page-redirect-code-location
+// @namespace   https://github.com/CC11001100/page-redirect-code-location-hook
 // @version      0.1
 // @description  阻断页面跳转，留在当前页面分析
 // @author       CC11001100
 // @match       *://*/*
 // @run-at      document-start
 // @grant        none
+// @license MIT
 // ==/UserScript==
 (() => {
 
-    // 使用说明： https://github.com/CC11001100/crawler-js-hook-framework-public/tree/master/011-page-redirect-code-location
+    // 使用说明： https://github.com/CC11001100/page-redirect-code-location-hook
 
     window.onbeforeunload = () => {
         debugger;
         return false;
     }
+
 })();
