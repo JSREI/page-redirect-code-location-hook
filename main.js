62219fe5521cd82a375c3132dd2a114c433b4364
CC11001100 <CC11001100@qq.com>
2023-07-30 23:42:31 +0800

    add test case

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         页面跳转JS代码定位通杀方案
-// @namespace   https://github.com/CC11001100/page-redirect-code-location-hook
+// @namespace   https://github.com/JSREI/page-redirect-code-location-hook
 // @version      0.2
 // @description  阻断页面跳转，留在当前页面分析
 // @author       CC11001100
